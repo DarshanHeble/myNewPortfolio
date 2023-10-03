b9d45d03972367e7bb09cc6cfa4396e9d5bbc630
Darshan Heble <darshanheble@gmail.com>
2023-10-03 18:14:46 +0530

    .

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -115,95 +115,96 @@ function Navbar() {
             icon={faMoon}
           />
         </span>
-
-        {/* <span className="span1 bar">
-          <FontAwesomeIcon
-            className="fa-bar"
-            title="Main Menu"
-            icon={faEllipsisVertical}
-          />
-        </span> */}
       </div>
 
       {/* mobile menu  */}
       <div className="mobilemenu">
-        <span className="span1 bar">
+        <span className="span1 moon">
           <FontAwesomeIcon
-            className="fa-bar"
-            title="Main Menu"
-            icon={faEllipsisVertical}
+            className="fa-moon"
+            title="dark mode"
+            icon={faMoon}
           />
         </span>
-        <div className="mobilewrapper">
-          <Link
-            activeClass="active"
-            to="home"
-            offset={-20}
-            spy={true}
-            smooth={true}
-            // duration={900}
-            className="button b1"
-          >
-            <div className="icon icon1">
-              <FontAwesomeIcon icon={faHouseCrack} />
-            </div>
-            <div className="tooltip t1">Home</div>
-          </Link>
+        <div className="bar">
+          <span className="span1">
+            <FontAwesomeIcon
+              className="fa-bar"
+              title="Main Menu"
+              icon={faEllipsisVertical}
+            />
+          </span>
+          <div className="mobilewrapper">
+            <Link
+              activeClass="active"
+              to="home"
+              offset={-20}
+              spy={true}
+              smooth={true}
+              // duration={900}
+              className="button b1"
+            >
+              <div className="icon icon1">
+                <FontAwesomeIcon icon={faHouseCrack} />
+              </div>
+              <div className="tooltip t1">Home</div>
+            </Link>
 
-          <Link
-            activeClass="active"
-            to="about"
-            offset={-20}
-            spy={true}
-            smooth={true}
-            className="button b2"
-          >
-            <div className="icon icon2">
-              <FontAwesomeIcon icon={faUserTag} />
-            </div>
-            <div className="tooltip t2 ">About</div>
-          </Link>
+            <Link
+              activeClass="active"
+              to="about"
+              offset={-20}
+              spy={true}
+              smooth={true}
+              className="button b2"
+            >
+              <div className="icon icon2">
+                <FontAwesomeIcon icon={faUserTag} />
+              </div>
+              <div className="tooltip t2 ">About</div>
+            </Link>
 
-          <Link
-            activeClass="active"
-            to="skills"
-            offset={-20}
-            spy={true}
-            smooth={true}
-            className="button b3"
-          >
-            <div className="icon icon3">
-              <FontAwesomeIcon icon={faStarHalfStroke} />
-            </div>
-            <div className="tooltip t3 ">Skills</div>
-          </Link>
+            <Link
+              activeClass="active"
+              to="skills"
+              offset={-20}
+              spy={true}
+              smooth={true}
+              className="button b3"
+            >
+              <div className="icon icon3">
+                <FontAwesomeIcon icon={faStarHalfStroke} />
+              </div>
+              <div className="tooltip t3 ">Skills</div>
+            </Link>
 
-          <Link
-            activeClass="active"
-            to="projects"
-            offset={-20}
-            spy={true}
-            smooth={true}
-            className="button b4"
-          >
-            <div className="icon icon4">
-              <FontAwesomeIcon icon={faDiagramProject} />
-            </div>
-            <div className="tooltip t4">Projects</div>
-          </Link>
-          <Link
-            activeClass="active"
-            to="contact"
-            offset={-20}
-            spy={true}
-            smooth={true}
-            className="button b6"
-          >
-            <div className="icon icon6">
-              <FontAwesomeIcon icon={faMessage} />
-            </div>
-            <div className="tooltip t6">Contact</div>
-          </Link>
+            <Link
+              activeClass="active"
+              to="projects"
+              offset={-20}
+              spy={true}
+              smooth={true}
+              className="button b4"
+            >
+              <div className="icon icon4">
+                <FontAwesomeIcon icon={faDiagramProject} />
+              </div>
+              <div className="tooltip t4">Projects</div>
+            </Link>
+            <Link
+              activeClass="active"
+              to="contact"
+              offset={-20}
+              spy={true}
+              smooth={true}
+              className="button b6"
+            >
+              <div className="icon icon6">
+                <FontAwesomeIcon icon={faMessage} />
+              </div>
+              <div className="tooltip t6">Contact</div>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
